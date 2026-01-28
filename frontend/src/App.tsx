8b19ba5ad2404781767b3b4b63a35c31a6f19c28
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-01-28 22:51:15 +0530

    created a basic webpage for our Office Management System

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1 className="title">WORK</h1>
+
+      <p className="subtitle">
+        Office Management & Workforce Coordination Platform
+      </p>
+
+      <div className="work-box">
+        Core office systems will be managed here
+      </div>
+
+      <button className="primary-btn">Enter Workspace</button>
+    </div>
+  );
+};
+
+export default App;
